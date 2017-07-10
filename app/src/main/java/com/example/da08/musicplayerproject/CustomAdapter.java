153d6ccc57d5d70c0ee7eda991acf60c03796620
박다정 <daaa08@naver.com>
2017-07-11 00:26:32 +0900

    list setting

--- app/src/main/java/com/example/da08/musicplayerproject/CustomAdapter.java
+++ app/src/main/java/com/example/da08/musicplayerproject/CustomAdapter.java
@@ -1,33 +1,76 @@
 package com.example.da08.musicplayerproject;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.da08.musicplayerproject.domain.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Da08 on 2017. 7. 10..
  */
 
-public class CustomAdapter extends RecyclerView.ViewHolder{
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.Holder>{
+
+    Context context;
+
+    List<Data.Music> data = new ArrayList<>();
+
+    public CustomAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(List<Data.Music> data){  // 음악 목록 데이터 세팅
+        this.data = data;
+
+    }
 
 
-    public CustomAdapter(View itemView) {
-        super(itemView);
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent,false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        holder.position = position;
+        holder.txtTitle.setText(data.get(position).title);
+        holder.txtSinger.setText(data.get(position).artist);
+        Glide.with(context).load(data.get(position).albumArt).into(holder.imgAlbum);
+
     }
 
     class Holder extends RecyclerView.ViewHolder {
-        public ImageView imgAlbum;
+
+        public int position;
         public TextView txtTitle, txtSinger;
         public ImageButton btnMenu;
+        public ImageView imgAlbum;
 
         public Holder(View itemView) {
             super(itemView);
-            imgAlbum = (ImageView)itemView.findViewById(R.id.imgAlbum);
             txtTitle = (TextView)itemView.findViewById(R.id.txtTitle);
             txtSinger = (TextView)itemView.findViewById(R.id.txtSinger);
-            btnMenu = (ImageButton) itemView.findViewById(R.id.btnMenu);
+            imgAlbum = (ImageView)itemView.findViewById(R.id.imgAlbum);
+            btnMenu = (ImageButton)itemView.findViewById(R.id.btnMenu);
+
+
         }
 
     }
--- app/src/main/java/com/example/da08/musicplayerproject/PlayListActivity.java
+++ app/src/main/java/com/example/da08/musicplayerproject/PlayListActivity.java
@@ -1,12 +1,25 @@
 package com.example.da08.musicplayerproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.da08.musicplayerproject.domain.Data;
+
+import java.util.List;
 
 public class PlayListActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    TextView txtTitleL, txtSingerL;
+    ImageView imgAlbumL;
+    ImageButton btnPlayL;
+
+    CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,18 @@ public class PlayListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play_list);
 
         recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        txtTitleL = (TextView)findViewById(R.id.txtSingerL);
+        txtSingerL = (TextView)findViewById(R.id.txtSingerL);
+        imgAlbumL = (ImageView)findViewById(R.id.imgAlbumL);
+        btnPlayL = (ImageButton)findViewById(R.id.btnPlayL);
+
+        adapter = new CustomAdapter(this);
+
+        List<Data.Music>data = Data.read(this);  // 데이터 가져옴
+        adapter.setData(data);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 }
